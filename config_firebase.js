4290e78aeb53e002c11eae1133bc738582040ab4
GRIMMETT <maximilian.a.grimmett@durham.ac.uk>
2019-02-11 10:31:50 +0000

    Added firebase configuration file.

---
+++ config_firebase.js
@@ -0,0 +1,7 @@
+"use strict";
+module.exports = {
+    apiKey: "<API_KEY>",
+    authDomain: "<PROJECT_ID>.firebaseapp.com",
+    databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+    storageBucket: "<BUCKET>.appspot.com",
+};
