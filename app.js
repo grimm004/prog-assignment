2a0a51f01071e75ae8aa79636b8759e84118330b
grimm004 <grimm004@gmail.com>
2019-04-03 22:04:18 +0100

    Added main page design,

--- app.js
+++ app.js
@@ -1,68 +1,37 @@
 "use strict";
 
-const forceOnlineFirebase = true;
-var environment = process.env.NODE_ENV || 'development';
 var express = require("express");
-var socket = require("socket.io");
-var firebase = null;
-if (environment === "production" || forceOnlineFirebase) {
-    firebase = require("firebase");
-    var firebaseConfig = require("./config_firebase");
-    firebase.initializeApp(firebaseConfig);
-} else
-    firebase = require("./mock_firebase");
+var http = require("http");
+var socketio = require("socket.io");
 
 class ChatApplication {
     constructor(port, public_folder = "public") {
         this._port = port;
 
         this._app = express();
-        this._app.use(express.static(public_folder, { extensions: ['html', 'htm'], }));
+        this._http = http.Server(this._app);
+        this._io = socketio(this._http);
+
+        // Express app setup
+        if (public_folder)
+            this._app.use(express.static(public_folder, { extensions: ['html', 'htm'], }));
         this._app.use(express.json());
-
-        this._app.post("/login", (req, res) => {
-            console.log("Login Requested");
-            firebase.auth().signInWithEmailAndPassword(req.body.email, req.body.password)
-                .then(user => {
-                    console.log("Success...");
-                    res.json({ loggedIn: user != null, errorCode: "" });
-                })
-                .catch(function (error) {
-                    console.log("Error logging in '" + req.body.email + "': " + error.code + ", " + error.message);
-                    res.json({ loggedIn: false, errorCode: error.code });
-                });
-        });
-
-        this._app.post("/register", (req, res) => {
-            console.log("Register Request: " + req.body.email);
-
-            firebase.auth().createUserWithEmailAndPassword(req.body.email, req.body.password)
-                .then(() => res.json({ registered: true, errorCode: "" }))
-                .catch(function (error) {
-                    console.log("Error registering '" + req.body.email + "': " + error.code + ", " + error.message);
-                    res.json({ registered: false, errorCode: error.code });
-                });
-        });
-
-        this._app.post("/status",
-            (_, res) => res.json({ loggedIn: firebase.auth().currentUser != null }));
-
-        this._app.post("/logout",
-            (_, res) => firebase.auth().signOut()
-                .then(() => res.status(200).json({}))
-                .catch(error => console.log("Error handling logout request: " + error)));
     }
 
     get Port() {
         return this._port;
     }
 
-    get ExpressInstance() {
+    get Express() {
         return this._app;
     }
 
+    get SocketIO() {
+        return this._io;
+    }
+
     start() {
-        this._app.listen(this._port);
+        this._http.listen(this._port, () => console.log("Started ProgNodeChat server on port " + this.Port + "..."));
     }
 }
 
